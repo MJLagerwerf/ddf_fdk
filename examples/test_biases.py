c9d61639cf97f62b5511e661a9212a0d44554d07
Rien Lagerwerf <rienlagerwerf@gmail.com>
2019-10-30 15:27:26 +0100

    Test biases script2

--- examples/test_biases.py
+++ examples/test_biases.py
@@ -16,9 +16,14 @@ ddf.import_astra_GPU()
 import time
 import pylab
 import gc
+import os
 from tqdm import tqdm
 pylab.close('all')
 t = time.time()
+
+
+
+
 # %% Set variables
 # The size of the measured objects in voxels
 pix = 256
@@ -37,12 +42,26 @@ angles = 500
 nTests = 500
 
 # %%
+data_obj = ddf.phantom(voxels, 'FORBILD', angles, noise, src_rad, det_rad,
+                       samp_fac=1)
+
+## %% Create the circular cone beam CT class
+case = ddf.CCB_CT(data_obj)#
+## Initialize the algorithms (FDK, SIRT)
+case.init_algo()
+case.init_DDF_FDK()
+case.TFDK.do(lam='optim')
+x_FB = case.TFDK.results.var[-1]
+
+# %%
 CS_T = np.zeros((nTests, pix))
+CS_FB = np.zeros((nTests, pix))
 CS_RL = np.zeros((nTests, pix))
 CS_G8 = np.zeros((nTests, pix))
 CS_B5 = np.zeros((nTests, pix))
 
 LS_T = np.zeros((nTests, pix))
+LS_FB = np.zeros((nTests, pix))
 LS_RL = np.zeros((nTests, pix))
 LS_G8 = np.zeros((nTests, pix))
 LS_B5 = np.zeros((nTests, pix))
@@ -58,7 +77,6 @@ def save_results(av_rec, CS, LS, path, meth):
     np.save(f'{path}_LS_{meth}', LS)
 
 
-    
 # %%
 for i in tqdm(range(nTests)):
     data_obj = ddf.phantom(voxels, phantom, angles, noise, src_rad, det_rad,
@@ -79,10 +97,12 @@ for i in tqdm(range(nTests)):
         rec_B5 = case.FDK.filt_LP('Shepp-Logan', ['Bin', 5],
                                compute_results=False)
         rec_T = case.FDK_bin(x)
+        rec_FB = case.FDK_bin(x_FB)
         add_results(CS_RL, LS_RL, rec_RL, i)
         add_results(CS_G8, LS_G8, rec_G8, i)
         add_results(CS_B5, LS_B5, rec_B5, i)
         add_results(CS_T, LS_T, rec_T, i)
+        add_results(CS_FB, LS_FB, rec_FB, i)
 
     else:
         rec = case.FDK.do('Ram-Lak', compute_results=False)
@@ -102,11 +122,16 @@ for i in tqdm(range(nTests)):
         rec = case.FDK_bin(x)
         add_results(CS_T, LS_T, rec, i)
         rec_T += rec
+        
+        rec = case.FDK_bin(x_FB)
+        add_results(CS_FB, LS_FB, rec, i)
+        rec_FB += rec
 
 rec_RL /= nTests
 rec_G8 /= nTests
 rec_B5 /= nTests
 rec_T /= nTests
+rec_FB /= nTests
     
 
 # %%
@@ -117,7 +142,7 @@ def compute_sd(CS, CS_av):
     return np.sqrt(S / np.shape(CS)[0])
 
 # %%
-def plot_slice(GT, av_recs, sd_recs, meths):
+def plot_slice(GT, av_recs, sd_recs, meths, path=None):
     pylab.figure()
     pylab.plot(GT, label='GT', lw=4)
     x = np.arange(len(GT))
@@ -127,36 +152,45 @@ def plot_slice(GT, av_recs, sd_recs, meths):
         pylab.errorbar(x, av_recs[i], sd_recs[i], label=meths[i], lw=2,
                         errorevery=20, capsize=5)
     pylab.legend()
+
 # %%  
 pylab.close('all')
-meths = ['RL', 'G8', 'B5', 'T']
+meths = ['RL', 'G8', 'B5', 'T', 'FB']
 # %%
 GT_CS = data_obj.f[:, pix // 2, pix // 2]
 av_recs_CS = [rec_RL[:, pix // 2, pix //2], rec_G8[:, pix // 2, pix //2],
-           rec_B5[:, pix // 2, pix //2], rec_T[:, pix // 2, pix //2]]
+           rec_B5[:, pix // 2, pix //2], rec_T[:, pix // 2, pix //2],
+           rec_FB[:, pix // 2, pix //2]]
 sd_recs_CS = [compute_sd(CS_RL, rec_RL[:, pix // 2, pix //2]), 
            compute_sd(CS_G8, rec_G8[:, pix // 2, pix //2]),
            compute_sd(CS_B5, rec_B5[:, pix // 2, pix //2]),
-           compute_sd(CS_T, rec_T[:, pix // 2, pix //2])]
-
-
+           compute_sd(CS_T, rec_T[:, pix // 2, pix //2]),
+           compute_sd(CS_FB, rec_FB[:, pix // 2, pix //2])]
 
 plot_slice(GT_CS, av_recs_CS, sd_recs_CS, meths)
+
 # %%
 GT_LS = data_obj.f[pix // 2, pix // 2, :]
 av_recs_LS = [rec_RL[pix // 2, pix //2, :], rec_G8[pix // 2, pix //2, :],
-           rec_B5[pix // 2, pix //2, :], rec_T[pix // 2, pix //2, :]]
+           rec_B5[pix // 2, pix //2, :], rec_T[pix // 2, pix //2, :],
+           rec_FB[pix // 2, pix //2, :]]
 sd_recs_LS = [compute_sd(CS_RL, rec_RL[pix // 2, pix //2, :]), 
            compute_sd(CS_G8, rec_G8[pix // 2, pix //2, :]),
            compute_sd(CS_B5, rec_B5[pix // 2, pix //2, :]),
-           compute_sd(CS_T, rec_T[pix // 2, pix //2, :])]
-
-
+           compute_sd(CS_T, rec_T[pix // 2, pix //2, :]),
+           compute_sd(CS_FB, rec_FB[pix // 2, pix //2, :])]
 
 plot_slice(GT_LS, av_recs_LS, sd_recs_LS, meths)
+
 # %%
 path = '/export/scratch2/lagerwer/AFFDK_results/resubmission/bias/'
-
+if not os.path.exists(path):
+    os.makedirs(path)
+save_results(rec_RL, CS_RL, LS_RL, path, 'RL')
+save_results(rec_G8, CS_G8, LS_G8, path, 'G8')
+save_results(rec_B5, CS_B5, LS_B5, path, 'B5')
+save_results(rec_T, CS_T, LS_T, path, 'T')
+save_results(rec_FB, CS_FB, LS_FB, path, 'FB')
 
 
 # %%
