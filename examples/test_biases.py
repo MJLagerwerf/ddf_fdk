7bc91d98474bece142fb019f3e87ed4663b25060
Rien Lagerwerf <rienlagerwerf@gmail.com>
2019-11-05 15:02:24 +0100

    Updated the test biases5

--- examples/test_biases.py
+++ examples/test_biases.py
@@ -31,7 +31,7 @@ voxels = [pix, pix, pix]
 
 # Pick your phantom
 # Options: 'Shepp-Logan', 'Defrise', 'Derenzo', 'Hollow cube', 'Cube', 'Var obj'
-phantom = 'cylinder ramp'
+phantom = 'cylinder'
 if phantom == 'FORBILD':
     PH = 'FB'
 if phantom == 'cylinder ramp':
