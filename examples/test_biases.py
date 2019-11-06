56caa39f504df78ef977144b759bc1b226b365fe
Rien Lagerwerf <rienlagerwerf@gmail.com>
2019-11-06 14:23:34 +0100

    CS phantom

--- examples/test_biases.py
+++ examples/test_biases.py
@@ -31,7 +31,7 @@ voxels = [pix, pix, pix]
 
 # Pick your phantom
 # Options: 'Shepp-Logan', 'Defrise', 'Derenzo', 'Hollow cube', 'Cube', 'Var obj'
-phantom = 'FORBILD'
+phantom = 'Cluttered sphere'
 if phantom == 'FORBILD':
     PH = 'FB'
 if phantom == 'cylinder ramp':
